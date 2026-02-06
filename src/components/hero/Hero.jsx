86e743866976f962c4be670e117d0a1d46757da5
LidiiaPopadiuk <lidiiadolfin@gmail.com>
2026-02-07 00:47:48 +0100

    finish with hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,6 @@
 import x from './Hero.module.scss'
 import { useState } from 'react'
+import { IoSearch } from "react-icons/io5";
 
 export const Hero = () => {
 
@@ -10,13 +11,23 @@ export const Hero = () => {
 
     return (
         <div className={x.hero}>
-            <h1>Weather dashboard</h1>
-            <div>
-                <p>Create your personal list of favorite cities and always be aware of the weather.</p>
-                <div></div>
-                <p>{month} {year}</p>
-                <p>{day} {date} </p>
-            </div>
+            <div className='container'>
+               {/* <div className={x.hero__titleWrapper}> */}
+                 <h1 className={x.hero__title}>Weather dashboard</h1>
+                <div className={x.hero__wrapper}>
+                    <p className={x.hero__parag}>Create your personal list of favorite cities and always be aware of the weather.</p>
+                    <div className={x.hero__bar}></div>
+                    <div className={x.hero__date}>
+                        <p className={x.hero__paragg}>{month} {year}</p>
+                        <p className={x.hero__paragg}>{day} {date} </p>
+                    </div>
+                </div>
+               </div>
+                <form className={x.hero__form}>
+                    <input placeholder='Search location...' className={x.hero__input} type="submite" />
+                    <button className={x.hero__btn}><IoSearch /></button>
+                </form>
+            {/* </div> */}
         </div>
     )
 }
