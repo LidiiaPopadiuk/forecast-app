db725a3423a79239e15fd374fd8dd1276a3b656a
LidiiaPopadiuk <lidiiadolfin@gmail.com>
2026-02-08 16:07:28 +0100

    doing news

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Header } from './components/header/Header';
 import { Hero } from './components/hero/Hero';
 import { Cards } from './components/cards/Cards';
 import { useFetch } from './hooks/useFetch';
+import { News } from './components/news/News';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Header></Header>
       <Hero inputInfo={inputInfo}></Hero>
       <Cards infoCity={infoCity}></Cards>
+      <News></News>
     </div>
   );
 }
--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,1 +1,18 @@
-export const News = () => {}
+import x from './News.module.scss'
+
+export const News = () => {
+    return (
+        <div className={x.news}>
+            <div className="container">
+                <h2 className={x.news__title}>Interacting with our pets</h2>
+                <ul className={x.news__list}>
+                    <li className={x.news__item}>
+                        <img src="" alt="" />
+                        <p><a href=""></a></p>
+                    </li>
+                </ul>
+                <button className={x.news__btn}>See more</button>
+            </div>
+        </div>
+    )
+}
