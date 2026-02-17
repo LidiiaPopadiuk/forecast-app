c69f8c4d5abe3c3369e67bdf27f426b757b4e9c3
LidiiaPopadiuk <lidiiadolfin@gmail.com>
2026-02-17 14:42:36 +0100

    aditing news

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -7,9 +7,12 @@ export const News = ({ petsInfo }) => {
                 <h2 className={x.news__title}>Interacting with our pets</h2>
                 <ul className={x.news__list}>
                     {petsInfo.map((pet, index) => {
+                        const link = () => {
+                            return pet.url
+                        }
                         return (
-                            <li key={index} className={x.news__item}>
-                                 <img className={x.news__img} src={pet.urlToImage} alt={pet.author} />
+                            <li onClick={link()} key={index} className={x.news__item}>
+                                <img className={x.news__img} src={pet.urlToImage} alt={pet.title} />
                                 <p className={x.news__parag}><a target='_blank' className={x.news__link} href={pet.url}>{pet.title}</a></p>
                             </li>
                         )
--- src/hooks/useNews.js
+++ src/hooks/useNews.js
@@ -4,8 +4,20 @@ import axios from "axios";
 export const useNews = () => {
   const [news, setNews] = useState([]);
 
+  const qRequests = ['pet', 'pets', 'cat', 'dog', 'animals', 'birds', 'fish', 'lizard', 'pinguin', 'dolfin', 'cow', 'farm animals']
+  
+  const whatAnimal = () => {
+    const randomIndex = Math.floor(Math.random() * qRequests.length)
+    return qRequests[randomIndex]
+  }
+
   const API =
-    "https://newsapi.org/v2/everything?q=pet&apiKey=291747d429c64cd3a73b4b56ac5c9cf3";
+    `https://newsapi.org/v2/everything?q=${whatAnimal()}&language=en&apiKey=291747d429c64cd3a73b4b56ac5c9cf3`;
+
+      // const API = `https://pixabay.com/api/?key=53835167-c7d1482498fed66d7f39b6868&q=${whatAnimal()}&image_type=photo`
+
+      // const API = `https://gnews.io/api/v4/search?q=${whatAnimal()}&lang=en&max=8&apikey=adf55da63854e7af9c4124c0888be32c`
+
 
   const getAPI = async () => {
     try {
