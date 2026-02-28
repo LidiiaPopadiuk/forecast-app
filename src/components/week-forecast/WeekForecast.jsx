5ac9f7267cc041e0f47b84b20707207ac1e948bd
LidiiaPopadiuk <lidiiadolfin@gmail.com>
2026-02-28 19:54:19 +0100

    finish with week forecast

--- src/components/week-forecast/WeekForecast.jsx
+++ src/components/week-forecast/WeekForecast.jsx
@@ -1,5 +1,15 @@
 import x from './WeekForecast.module.scss'
+import { useState, useEffect } from 'react';
 export const WeekForecast = ({ infoForecast }) => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+    useEffect(() => {
+        const onResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+
+        window.addEventListener('resize', onResize);
+        return () => window.removeEventListener('resize', onResize);
+    }, []);
 
     if (!infoForecast || infoForecast.length === 0) return null
 
@@ -9,7 +19,7 @@ export const WeekForecast = ({ infoForecast }) => {
         <div className={x.week}>
             <div className="container">
                 <div className={x.week__wrap}>
-                    <h2 className={x.week__title}>5-day forecast</h2>
+                    <h2 className={x.week__title}>{isMobile ? "4-Day Forecast" : "5-Day Forecast"}</h2>
                     <ul className={x.week__list}>
                         {dailyForecast.map((day, index) => {
 
@@ -25,7 +35,7 @@ export const WeekForecast = ({ infoForecast }) => {
 
                             return (
                                 <li key={index} className={x.week__item}>
-                                    <p className={x.week__day}>
+                                    <p className={x.week__date}>
                                         {date.toLocaleDateString("en-US", {
                                             weekday: "short",
                                             month: "short",
@@ -38,7 +48,7 @@ export const WeekForecast = ({ infoForecast }) => {
                                             {Math.round(maxTemp)}/{Math.round(minTemp)}℃
                                         </p>
                                     </div>
-                                    <p className={x.week__day}>{day.weather[0].description}</p>
+                                    <p className={x.week__descr}>{day.weather[0].description}</p>
                                 </li>
                             )
                         })}
@@ -48,60 +58,3 @@ export const WeekForecast = ({ infoForecast }) => {
         </div>
     )
 }
-
-// <li className={x.week__item}>
-//         <p className={x.week__day}>Sat, Oct 14</p>
-//         <div className={x.week__wrapper}>
-//             <img src={svgRain} alt="" />
-//             <p className={x.week__degree}>22/10℃</p>
-//         </div>
-//         <p className={x.week__day}>{day.weather[7].description}</p>
-//     </li>
-//     <li className={x.week__item}>
-//         <p className={x.week__day}>Sun, Oct 15</p>
-//         <div className={x.week__wrapper}>
-//             <img src={svgRain} alt="" />
-//             <p className={x.week__degree}>13/6℃</p>
-//         </div>
-//         <p className={x.week__day}>{day.weather[14].description}</p>
-//     </li>
-//     <li className={x.week__item}>
-//         <p className={x.week__day}>Mon, Oct 16</p>
-//         <div className={x.week__wrapper}>
-//             <img src={svgRain} alt="" />
-//             <p className={x.week__degree}>12/4℃</p>
-//         </div>
-//         <p className={x.week__day}>{day.weather[21].description}</p>
-//     </li>
-//     <li className={x.week__item}>
-//         <p className={x.week__day}>Tue, Oct 17</p>
-//         <div className={x.week__wrapper}>
-//             <img src={svgRain} alt="" />
-//             <p className={x.week__degree}>12/4℃</p>
-//         </div>
-//         <p className={x.week__day}>{day.weather[28].description}</p>
-//     </li>
-//     <li className={x.week__item}>
-//         <p className={x.week__day}>Wed, Oct 18</p>
-//         <div className={x.week__wrapper}>
-//             <img src={svgRain} alt="" />
-//             <p className={x.week__degree}>13/3℃</p>
-//         </div>
-//         <p className={x.week__day}>{day.weather[35].description}</p>
-//     </li>
-//     <li className={x.week__item}>
-//         <p className={x.week__day}>Thu, Oct 19</p>
-//         <div className={x.week__wrapper}>
-//             <img src={svgRain} alt="" />
-//             <p className={x.week__degree}>12/5℃</p>
-//         </div>
-//         <p className={x.week__day}>{day.weather[42].description}</p>
-//     </li>
-//     <li className={x.week__item}>
-//         <p className={x.week__day}>Fri, Oct 20</p>
-//         <div className={x.week__wrapper}>
-//             <img src={svgRain} alt="" />
-//             <p className={x.week__degree}>9/3℃</p>
-//         </div>
-//         <p className={x.week__day}>{day.weather[49].description}</p>
-//     </li>
